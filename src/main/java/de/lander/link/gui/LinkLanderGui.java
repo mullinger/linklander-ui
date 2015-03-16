43d22774cbd1b750b577e0d3d274afbbb1ea8101
max <max@ullinger.ch>
2015-03-16 12:38:33 +0100

    Extracted ui elements for search into dedicated component to easily parallelize development

--- src/main/java/de/lander/link/gui/LinkLanderGui.java
+++ src/main/java/de/lander/link/gui/LinkLanderGui.java
@@ -1,28 +1,13 @@
 package de.lander.link.gui;
 
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
-import org.apache.logging.log4j.Logger;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.cdi.CDIUI;
-import com.vaadin.event.FieldEvents.TextChangeEvent;
-import com.vaadin.event.FieldEvents.TextChangeListener;
-import com.vaadin.server.ExternalResource;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.Table.ColumnHeaderMode;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-import de.lander.persistence.daos.PersistenceGateway;
-import de.lander.persistence.entities.Link;
+import de.lander.link.gui.components.SearchComponent;
 
 @CDIUI("linklander-ui")
 @Theme("mytheme")
@@ -30,103 +15,16 @@ import de.lander.persistence.entities.Link;
 public class LinkLanderGui extends UI {
 
 	private VerticalLayout verticalLayout;
-	private Table links;
-	private TextField input;
 
-	@Inject
-	private Logger LOGGER;
-
-	@Inject
-	private PersistenceGateway persistenceGatewayImpl;
-
-	// @WebServlet(value = "/*", asyncSupported = true)
-	// @VaadinServletConfiguration(productionMode = false, ui =
-	// LinkLanderGui.class, widgetset = "de.lander.link.gui.AppWidgetSet")
-	// public static class Servlet extends VaadinServlet {
-	// }
-
-	@PostConstruct
-	public void postConstruct() {
-		persistenceGatewayImpl.addLink("Name", "http://name.de", "name");
-	}
 
 	@Override
 	protected void init(VaadinRequest request) {
-		buildLayout();
-
-		input.focus();
-		input.setInputPrompt("type to land a link...");
-		input.setTextChangeEventMode(TextChangeEventMode.LAZY);
-
-		links.addContainerProperty("name", String.class, null);
-		links.addContainerProperty("link", Component.class, null);
-		
-		
-		links.setColumnHeaderMode(ColumnHeaderMode.HIDDEN);
-		links.setFooterVisible(false);
-
-		input.addTextChangeListener(new TextChangeListener() {
-
-			@Override
-			public void textChange(final TextChangeEvent event) {
-				if (event.getText().equals("")) {
-					links.setVisible(false);
-					links.removeAllItems();
-				} else {
-					loadLinks(event.getText());
-					links.setVisible(true);
-				}
-			}
-
-		});
-
-	}
-
-	private void loadLinks(final String searchText) {
-		links.removeAllItems();
-
-		List<Link> searchLinks = persistenceGatewayImpl.searchLinks(PersistenceGateway.LinkProperty.NAME, searchText);
-		LOGGER.error("Found " + searchLinks.size() + " links");
-
-		for (int i = 0; i < searchLinks.size(); i++) {
-			LOGGER.error("Found link " + i + " " + searchLinks.get(i).toString());
-			links.addItem(convert(searchLinks.get(i)), i);
-		}
-	}
-
-	private Object[] convert(Link link) {
-		// Create a Vaading HTTP clickable link
-		com.vaadin.ui.Link externalLink = new com.vaadin.ui.Link(link.getUrl(), new ExternalResource(link.getUrl()));
-		externalLink.setTargetName("_blank"); // Open in new Tab
-
-		// Create the table row object array
-		return new Object[] { link.getName(), externalLink};
-	}
-
-	private void buildLayout() {
 		verticalLayout = new VerticalLayout();
 		verticalLayout.setWidth("100.0%");
 
-		// top-level component properties
-		setWidth("100.0%");
-		setHeight("100.0%");
-
-		// input
-		input = new TextField();
-		input.setImmediate(true);
-		input.setWidth("100.0%");
-		input.setHeight("-1px");
-		verticalLayout.addComponent(input);
-
-		// links
-		links = new Table();
-		links.setImmediate(false);
-		links.setWidth("100.0%");
-		// Important to avoid "the thin grey table bottom line"
-		links.setPageLength(0);
-		links.setVisible(false);
-		verticalLayout.addComponent(links);
-
+//		// top-level component properties
+		verticalLayout.addComponent(new SearchComponent());
+		
 		setContent(verticalLayout);
 	}
 
---
+++ src/main/java/de/lander/link/gui/components/SearchComponent.java
@@ -0,0 +1,115 @@
+package de.lander.link.gui.components;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.logging.log4j.Logger;
+
+import com.vaadin.server.ExternalResource;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomComponent;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.AbstractTextField.TextChangeEventMode;
+import com.vaadin.ui.Table.ColumnHeaderMode;
+
+import de.lander.persistence.daos.PersistenceGateway;
+import de.lander.persistence.entities.Link;
+
+public class SearchComponent extends CustomComponent {
+
+	private static final long serialVersionUID = 242479147429347833L;
+
+	private VerticalLayout verticalLayout;
+	private Table links;
+	private TextField input;
+
+	@Inject
+	private Logger LOGGER;
+
+	@Inject
+	private PersistenceGateway persistenceGatewayImpl;
+
+	@PostConstruct
+	public void postConstruct() {
+		 persistenceGatewayImpl.addLink("Name", "http://name.de", "name");
+	}
+	
+	public SearchComponent() {
+		buildLayout();
+
+		input.focus();
+		input.setInputPrompt("type to land a link...");
+		input.setTextChangeEventMode(TextChangeEventMode.LAZY);
+
+		links.addContainerProperty("name", String.class, null);
+		links.addContainerProperty("link", Component.class, null);
+
+		links.setColumnHeaderMode(ColumnHeaderMode.HIDDEN);
+		links.setFooterVisible(false);
+
+		input.addTextChangeListener(event -> {
+			if (event.getText().equals("")) {
+				links.setVisible(false);
+				links.removeAllItems();
+			} else {
+				loadLinks(event.getText());
+				links.setVisible(true);
+			}
+		});
+
+	}
+
+	private void loadLinks(final String searchText) {
+		links.removeAllItems();
+
+		List<Link> searchLinks = persistenceGatewayImpl.searchLinks(PersistenceGateway.LinkProperty.NAME, searchText);
+		LOGGER.error("Found " + searchLinks.size() + " links");
+
+		for (int i = 0; i < searchLinks.size(); i++) {
+			LOGGER.error("Found link " + i + " " + searchLinks.get(i).toString());
+			links.addItem(convert(searchLinks.get(i)), i);
+		}
+	}
+
+	private Object[] convert(Link link) {
+		// Create a Vaading HTTP clickable link
+		com.vaadin.ui.Link externalLink = new com.vaadin.ui.Link(link.getUrl(), new ExternalResource(link.getUrl()));
+		externalLink.setTargetName("_blank"); // Open in new Tab
+
+		// Create the table row object array
+		return new Object[] { link.getName(), externalLink };
+	}
+
+	private void buildLayout() {
+		verticalLayout = new VerticalLayout();
+		verticalLayout.setWidth("100.0%");
+
+		// top-level component properties
+		setWidth("100.0%");
+		setHeight("100.0%");
+
+		// input
+		input = new TextField();
+		input.setImmediate(true);
+		input.setWidth("100.0%");
+		input.setHeight("-1px");
+		verticalLayout.addComponent(input);
+
+		// links
+		links = new Table();
+		links.setImmediate(false);
+		links.setWidth("100.0%");
+		// Important to avoid "the thin grey table bottom line"
+		links.setPageLength(0);
+		links.setVisible(false);
+		verticalLayout.addComponent(links);
+
+		setCompositionRoot(verticalLayout);
+	}
+}
+
+
