8a52d66c5a613c4862be0905516db1a66c64a4d4
max <max@ullinger.ch>
2015-10-14 22:02:15 +0200

    Allow to configure tags for a link directly from the edit/add link window

--- src/main/java/de/lander/link/gui/components/EditLinkWindow.java
+++ src/main/java/de/lander/link/gui/components/EditLinkWindow.java
@@ -1,12 +1,20 @@
 package de.lander.link.gui.components;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import com.vaadin.ui.Button;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.Window;
 
+import de.lander.link.gui.components.tag.SelectTagWindow;
 import de.lander.persistence.daos.PersistenceGateway;
 import de.lander.persistence.entities.Link;
+import de.lander.persistence.entities.Tag;
 
 public class EditLinkWindow extends Window {
 
@@ -19,6 +27,11 @@ public class EditLinkWindow extends Window {
 	private TextField nameField;
 
 	private TextField urlField;
+
+	private Button editTagsButtong;
+	private Set<String> addedTags = new HashSet<String>();
+	private Set<String> removedTags = new HashSet<String>();
+	private SelectTagWindow selectTagWindow = null;
 
 	private Button saveButton;
 
@@ -74,12 +87,63 @@ public class EditLinkWindow extends Window {
 		titleField.setWidth("100%");
 		root.addComponent(titleField);
 
+		// Edit Tags button
+		editTagsButtong = new Button("Edit Tags");
+		editTagsButtong.addClickListener(event -> {
+			editTags();
+		});
+		root.addComponent(editTagsButtong);
+
 		// Save Button
 		saveButton = new Button("Save");
 		saveButton.addClickListener(event -> {
 			saveLink();
 		});
 		root.addComponent(saveButton);
+	}
+
+	private void editTags() {
+		Set<String> previousUuidSet = new HashSet<String>();
+		if (existingLinkId == null) {
+			// New Link -> No Tags preselected
+			selectTagWindow = new SelectTagWindow(persistence, addedTags);
+		} else {
+			// Existing Link -> Load tags
+			List<Tag> tagsForLink = persistence.getTagsForLink(existingLinkId);
+			previousUuidSet.addAll(tagsForLink.stream().map(t -> t.getUuid()).collect(Collectors.toSet()));
+
+			// Calculate the current (possibly not ye persisted) selection of
+			// tags
+			HashSet<String> currentSelection = new HashSet<String>();
+			currentSelection.addAll(previousUuidSet);
+			currentSelection.addAll(addedTags);
+			currentSelection.removeAll(removedTags);
+
+			selectTagWindow = new SelectTagWindow(persistence, currentSelection);
+		}
+
+		selectTagWindow.addCloseListener(event -> {
+			/*
+			 * Store the changed tags in this instance of editLinkWindow. if the
+			 * user cancels we just ignore the changes if the user saves we
+			 * store the changed tags in he persistence.
+			 */
+			Set<String> selectedTagUUIDs = selectTagWindow.getSelectedTagUUIDs();
+
+			// Added Tags:
+				this.addedTags = new HashSet<String>();
+				this.addedTags.addAll(selectedTagUUIDs);
+				this.addedTags.removeAll(previousUuidSet);
+
+				// Removed tags
+				this.removedTags = new HashSet<String>();
+				this.removedTags.addAll(previousUuidSet);
+				this.removedTags.removeAll(selectedTagUUIDs);
+			});
+
+		// Open the window
+		UI.getCurrent().addWindow(selectTagWindow);
+
 	}
 
 	private void initData() {
@@ -98,11 +162,23 @@ public class EditLinkWindow extends Window {
 
 	private void saveLink() {
 		if (existingLinkId == null) {
-			persistence.addLink(nameField.getValue(), urlField.getValue(), titleField.getValue());
+			// New Link
+			String linkUUID = persistence.addLink(nameField.getValue(), urlField.getValue(), titleField.getValue());
+			for (String tagUUID : addedTags) {
+				persistence.addTagToLink(linkUUID, tagUUID);
+			}
 		} else {
+			// Update Link
 			persistence.setLinkPropertyValue(existingLinkId, Link.NAME, nameField.getValue());
 			persistence.setLinkPropertyValue(existingLinkId, Link.URL, urlField.getValue());
 			persistence.setLinkPropertyValue(existingLinkId, Link.TITLE, titleField.getValue());
+
+			for (String tagUUID : addedTags) {
+				persistence.addTagToLink(existingLinkId, tagUUID);
+			}
+			for (String tagUUID : removedTags) {
+				persistence.removeTagFromLink(existingLinkId, tagUUID);
+			}
 		}
 
 		if (saveCallback != null) {
